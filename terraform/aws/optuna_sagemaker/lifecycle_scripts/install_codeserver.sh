3d44333e382c3d58b1fbc78a1a05f8ef6d65ef42
YangWu1227 <yangwu2020@gmail.com>
2025-04-03 22:20:30 -0400

    feat: update lifecycle configuration scripts

--- terraform/aws/optuna_sagemaker/lifecycle_scripts/install_codeserver.sh
+++ terraform/aws/optuna_sagemaker/lifecycle_scripts/install_codeserver.sh
@@ -1,76 +1,59 @@
+#!/bin/bash
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-#!/bin/bash
+# Exit immediately if a command exits with a non-zero status -e
+# Treat unset variables as an error and exit immediately -u
+# Print each command to stderr before executing it (for debugging) -x
 set -eux
 
-###############
-#  VARIABLES  #
-###############
-
+# Sagemaker notebook currently uses amazon linux 2, which uses GLIBC 2.26, and code-server 4.16.1 is the last version that supports GLIBC 2.26
 CODE_SERVER_VERSION="4.16.1"
 CODE_SERVER_INSTALL_LOC="/home/ec2-user/SageMaker/.cs"
 XDG_DATA_HOME="/home/ec2-user/SageMaker/.xdg/data"
 XDG_CONFIG_HOME="/home/ec2-user/SageMaker/.xdg/config"
-INSTALL_PYTHON_EXTENSION=1
 CREATE_NEW_CONDA_ENV=1
-CONDA_ENV_LOCATION='/home/ec2-user/SageMaker/.cs/conda/envs/codeserver_py39'
-CONDA_ENV_PYTHON_VERSION="3.9"
-INSTALL_DOCKER_EXTENSION=1
-USE_CUSTOM_EXTENSION_GALLERY=0
+CONDA_ENV_LOCATION="/home/ec2-user/SageMaker/.cs/conda/envs/codeserver_py311"
+CONDA_ENV_PYTHON_VERSION="3.11"
 
 sudo -u ec2-user -i <<EOF
 
 unset SUDO_UID
 
-#############
-#  INSTALL  #
-#############
-
-# set the data and config home env variable for code-server
+# Set the data and config home env variable for code-server
 export XDG_DATA_HOME=$XDG_DATA_HOME
 export XDG_CONFIG_HOME=$XDG_CONFIG_HOME
 export PATH="$CODE_SERVER_INSTALL_LOC/bin/:$PATH"
 
-# install code-server standalone
+# Install code-server standalone
 mkdir -p ${CODE_SERVER_INSTALL_LOC}/lib ${CODE_SERVER_INSTALL_LOC}/bin
 curl -fL https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz \
 | tar -C ${CODE_SERVER_INSTALL_LOC}/lib -xz
 mv ${CODE_SERVER_INSTALL_LOC}/lib/code-server-$CODE_SERVER_VERSION-linux-amd64 ${CODE_SERVER_INSTALL_LOC}/lib/code-server-$CODE_SERVER_VERSION
 ln -s ${CODE_SERVER_INSTALL_LOC}/lib/code-server-$CODE_SERVER_VERSION/bin/code-server ${CODE_SERVER_INSTALL_LOC}/bin/code-server
 
-# create separate conda environment
+# Create separate conda environment
 if [ $CREATE_NEW_CONDA_ENV -eq 1 ]
 then
     conda create --prefix $CONDA_ENV_LOCATION python=$CONDA_ENV_PYTHON_VERSION -y
 fi
 
-# install ms-python extension
-if [ $USE_CUSTOM_EXTENSION_GALLERY -eq 0 -a $INSTALL_PYTHON_EXTENSION -eq 1 ]
-then
-    code-server --install-extension ms-python.python --force
+# Install extensions (code-server has a different marketplace than VSCode)
+code-server --install-extension ms-python.python --force
+code-server --install-extension eamodio.gitlens --force
+code-server --install-extension njpwerner.autodocstring --force
+code-server --install-extension ms-toolsai.jupyter --force
+code-server --install-extension ms-vscode.cpptools-themes --force
+code-server --install-extension foxundermoon.shell-format --force
+code-server --install-extension redhat.vscode-yaml --force
+code-server --install-extension DavidAnson.vscode-markdownlint --force
 
-    # if the new conda env was created, add configuration to set as default
-    if [ $CREATE_NEW_CONDA_ENV -eq 1 ]
-    then
-        CODE_SERVER_MACHINE_SETTINGS_FILE="$XDG_DATA_HOME/code-server/Machine/settings.json"
-        if grep -q "python.defaultInterpreterPath" "\$CODE_SERVER_MACHINE_SETTINGS_FILE"
-        then
-            echo "Default interepreter path is already set."
-        else
-            cat >>\$CODE_SERVER_MACHINE_SETTINGS_FILE <<- MACHINESETTINGS
-{
-    "python.defaultInterpreterPath": "$CONDA_ENV_LOCATION/bin"
-}
-MACHINESETTINGS
-        fi
-    fi
+if command -v code-server &> /dev/null; then
+    INSTALLED_VERSION=\$(code-server --version | head -n 1)
+    echo "✅ code-server \$INSTALLED_VERSION successfully installed!"
+else
+    echo "❌ code-server installation verification failed!"
+    exit 1
 fi
-
-# install docker extension
-if [ $USE_CUSTOM_EXTENSION_GALLERY -eq 0 -a $INSTALL_DOCKER_EXTENSION -eq 1 ]
-then
-    code-server --install-extension ms-azuretools.vscode-docker --force
-fi
-
 EOF
