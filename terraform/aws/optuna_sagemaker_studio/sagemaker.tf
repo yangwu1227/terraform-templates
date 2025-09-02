ac06e1c2cf87d08f1daa37050be192e20ead25e6
YangWu1227 <yangwu2020@gmail.com>
2025-09-02 01:36:59 -0400

    refactor: set retention policy for sagemaker domain for efs

--- terraform/aws/optuna_sagemaker_studio/sagemaker.tf
+++ terraform/aws/optuna_sagemaker_studio/sagemaker.tf
@@ -13,6 +13,10 @@ resource "aws_sagemaker_domain" "sagemaker_domain" {
     security_group_ids = [aws_security_group.sagemaker_sg.id]
   }
 
+  retention_policy {
+    home_efs_file_system = "Delete"
+  }
+
   default_space_settings {
     execution_role  = aws_iam_role.sagemaker_execution_role.arn
     security_groups = [aws_security_group.sagemaker_sg.id]
